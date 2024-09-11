57b102ef9dc462470a75603a313495fab1806a64
ildaraaaaaaa <mmitir@list.ru>
2024-09-11 07:15:52 +0300

    TaskBSD.py

--- TaskBSD.py
+++ TaskBSD.py
@@ -1,8 +1,12 @@
 #1st program
-print((9 * 5) ** 0.5)
+print(5*(9**0.5))
 #2nd program
 print(9.99 > 9.98 and 1000 != 1000.1)
 #3rd program
-print((1234 % 1000) // 10 + (5678 % 1000) // 10)
+a = 2*2+2
+b = 2*(2+2)
+print(a == b)
 #4th program
-print(int(13.42) == int(42.13 * 100) % 100) # для чего в подсказке было сказано про функции 'or' и 'and'
+str_ = '123.456'
+str_ = float(str_) * 10
+print(int(str_) % 10)
